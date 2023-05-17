a4cdf87d774ff19f157406199d18d8e7e396e86f
Ali Zadil <tarikz94@gmail.com>
2023-05-17 18:11:53 +0300

    updated to 17th of may exercise

--- main-completed.js
+++ main-completed.js
@@ -4,7 +4,7 @@ const loadMemoryBtn = document.querySelector("#loadFromMemory");
 const container = document.querySelector(".container");
 
 const API_LINK = "https://jsonplaceholder.typicode.com/todos";
-
+const DATA_KEY_LOCALSTORAGE = 'todos';
 
 async function getData() {
     const response = await fetch(API_LINK);
@@ -13,6 +13,7 @@ async function getData() {
 }
 
 function renderData(data) {
+    container.innerHTML = '';
     for (let i = 0; i < data.length; i++) {
         const element = data[i];
 
@@ -20,18 +21,41 @@ function renderData(data) {
         divTag.className = "list-item";
         let h3Tag = document.createElement("h3");
         h3Tag.innerHTML = element.title;
-        let inputTag = document.createElement("input");
-        inputTag.className = "completed";
-        inputTag.type = "checkbox";
-        inputTag.checked = element.completed;
+        let checkboxTag = document.createElement("input");
+
+        checkboxTag.className = "completed";
+        checkboxTag.type = "checkbox";
+        checkboxTag.checked = element.completed;
+        checkboxTag.addEventListener("click", () => {
+            data[i].completed = checkboxTag.checked;
+            localStorage.setItem(DATA_KEY_LOCALSTORAGE, JSON.stringify(data));
+        })
+        let deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML = "Delete";
+        deleteBtn.addEventListener("click", () => {
+            data.splicec(i, 1);
+            renderData(data);
+            localStorage.setItem(DATA_KEY_LOCALSTORAGE, JSON.stringify(data));
+        })
 
         divTag.append(h3Tag);
         divTag.append(inputTag);
+        divTag.append(deleteBtn)
         container.append(divTag);
 
     }
 }
 
+async function loadInitialData(){
+    let localData = localStorage.getItem(DATA_KEY_LOCALSTORAGE);
+    if(localData === null){
+        let data = await getData();
+        renderData(data);
+    } else {
+        renderData(JSON.parse(localData));
+    }
+}
+loadInitialData();
 
 refreshBtn.addEventListener("click", async () => {
     let data = await getData();
@@ -40,10 +64,10 @@ refreshBtn.addEventListener("click", async () => {
 
 saveMemoryBtn.addEventListener("click", async () => {
     let data = await getData();
-    localStorage.setItem("todos", JSON.stringify(data));
+    localStorage.setItem(DATA_KEY_LOCALSTORAGE, JSON.stringify(data));
 });
 
 loadMemoryBtn.addEventListener("click", () => {
-    let data = JSON.parse(localStorage.getItem("todos"));
+    let data = JSON.parse(localStorage.getItem(DATA_KEY_LOCALSTORAGE));
     renderData(data);
 });
